5290df7ca1b646e262c1d1232c5e89e81599bc8d
Ryan Roemer <ryan.roemer@formidable.com>
2021-01-27 13:57:08 -0800

    Start serving serverless target

---
+++ server/index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const { createServer } = require("http");
+
+const page = require("../.next/serverless/pages/index.js");
+
+const DEFAULT_PORT = 3000;
+const PORT = parseInt(process.env.SERVER_PORT || DEFAULT_PORT, 10);
+const HOST = process.env.SERVER_HOST || "0.0.0.0";
+
+// Create the server app.
+const app = createServer((req, res) => page.render(req, res));
+
+// TODO: IMPLEMENT
+// // LAMBDA: Export handler for lambda use.
+// let handler;
+// module.exports.handler = (event, context, callback) => {
+//   // Lazy require `serverless-http` to allow non-Lambda targets to omit.
+//   // eslint-disable-next-line global-require
+//   handler = handler || require("serverless-http")(app);
+//   return handler(event, context, callback);
+// };
+
+// DOCKER/DEV/ANYTHING: Start the server directly.
+if (require.main === module) {
+  const server = app.listen({
+    port: PORT,
+    host: HOST
+  }, () => {
+    const { address, port } = server.address();
+
+    // eslint-disable-next-line no-console
+    console.log(`Server started at http://${address}:${port}`);
+  });
+}
