3f0f9c33822df1b0b7ea0ac0c2e2b6bc4a6dff10
Ryan Roemer <ryan.roemer@formidable.com>
2021-01-28 14:09:33 -0800

    Data works

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
-// TODO: HERE -- Refactor to express with static.
+const fs = require("fs").promises;
+const path = require("path");
 const express = require("express");
 
 const page = require("../.next/serverless/pages/index.js");
@@ -11,13 +12,35 @@ const HOST = process.env.SERVER_HOST || "0.0.0.0";
 const BASE_PATH = process.env.BASE_PATH || "/blog";
 const STAGE = process.env.STAGE || "localdev";
 
+const NEXT_DIR = path.resolve(__dirname, "../.next");
+const NEXT_APP_ROOT = "/_next";
+
 // Create the server app.
-const getApp = ({ basePath = "" } = {}) => {
+const getApp = async ({ basePath = "" } = {}) => {
+  const buildId = (await fs.readFile(path.join(NEXT_DIR, "BUILD_ID"))).toString().trim();
   const app = express();
+
+  // NOTE(STATIC): For this demo only, we just handle serve static content
+  // directly through the express app. For a real app, you'll want to deploy
+  // static contents to somewhere to be directly served by the CDN.
+  app.use(`${NEXT_APP_ROOT}/static`, express.static(path.join(NEXT_DIR, "static")));
+
+  // Proxy data requests.
+  // _next/data/y-BRZHyY6b_T25zMSRPY0/posts/ssg-ssr.json ->
+  // _next/serverless/posts/ssg-ssr.json
+  app.use(
+    `${NEXT_APP_ROOT}/data/${buildId}`,
+    express.static(path.join(NEXT_DIR, "serverless/pages"))
+  );
+
+  // Page handlers,
   app.all(`${basePath}`, (req, res) => {
-    console.log("TODO HERE REQ", req.url)
+    console.log("TODO HERE REQ", req.url);
     return page.render(req, res);
   });
+
+  // TODO: 404.
+  // TODO: Hook up error (?)
 
   return app;
 };
@@ -27,7 +50,7 @@ let handler;
 module.exports.handler = async (event, context) => {
   // Lazy require `serverless-http` to allow non-Lambda targets to omit.
   // eslint-disable-next-line global-require
-  handler = handler || require("serverless-http")(getApp({
+  handler = handler || require("serverless-http")(await getApp({
     basePath: BASE_PATH
   }));
 
@@ -36,13 +59,15 @@ module.exports.handler = async (event, context) => {
 
 // DOCKER/DEV/ANYTHING: Start the server directly.
 if (require.main === module) {
-  const server = getApp().listen({
-    port: PORT,
-    host: HOST
-  }, () => {
-    const { address, port } = server.address();
+  (async () => {
+    const server = (await getApp()).listen({
+      port: PORT,
+      host: HOST
+    }, () => {
+      const { address, port } = server.address();
 
-    // eslint-disable-next-line no-console
-    console.log(`Server started at http://${address}:${port}`);
-  });
+      // eslint-disable-next-line no-console
+      console.log(`Server started at http://${address}:${port}`);
+    });
+  })();
 }
